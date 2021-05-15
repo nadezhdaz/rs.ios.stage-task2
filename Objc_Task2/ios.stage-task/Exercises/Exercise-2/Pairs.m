374f3eb57886f3fb8151be54f908d5d170b918dd
nadyaios <nzenckova.ios@gmail.com>
2021-05-16 02:37:28 +0300

    feat: exercise 2 implemented

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,21 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+    NSInteger numberOfPairs = 0;
+    NSSortDescriptor *sortDescriptor = [NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES];
+    NSArray *sortedArray = [array sortedArrayUsingDescriptors:@[sortDescriptor]];
+    
+    for (NSNumber *numberA in sortedArray) {
+        NSUInteger indexOfNumberB = [sortedArray indexOfObject:numberA] + 1;
+        for (; indexOfNumberB < sortedArray.count; indexOfNumberB++) {
+            NSNumber *numberB = [sortedArray objectAtIndex:indexOfNumberB];
+            if ([numberA intValue] == [numberB intValue] - [number intValue]) {
+                numberOfPairs++;
+            }
+        }
+    }
+    
+    return numberOfPairs;
 }
 
 @end
