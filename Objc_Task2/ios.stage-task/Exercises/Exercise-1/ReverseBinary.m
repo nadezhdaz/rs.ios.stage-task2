92b59edac63afd563db69591293973b14054ebcf
nadyaios <nzenckova.ios@gmail.com>
2021-05-15 15:18:53 +0300

    feat: exercise 1 implemented

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,14 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 reversed = 0;
+
+        for (NSInteger i = 0; i < 8; i++) {
+            if (n % 2 == 1) {
+                reversed += pow(2, 7 - i);
+            }
+            n /= 2;
+        }
+    
+    return reversed;
 }
